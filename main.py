5034431e7b760c08b8301a02fa68c650fe74b83b
Dennis Weiss <weiss.dennis98@yahoo.de>
2018-12-05 10:04:18 +0100

    Added basic lux measuring and plottinh functionality

---
+++ main.py
@@ -0,0 +1,23 @@
+from tsl2561 import TSL2561
+import matplotlib.pyplot as plt
+
+
+tsl = TSL2561(debug=True)
+n = 10
+time_interval = 1
+
+
+def do_measurements(n, time_interval):
+    data = []
+    plt.ylabel('Lux')
+    plt.xlabel('time in s')
+    for i in range(n):
+        data.append(tsl.lux())
+        plt.plot(data)
+        plt.show()
+        
+    
+
+
+
+
